33009ba1ff1f4e196b1650d42e2dc92f7971153c
anikiti3 <anikiti3@email.cpcc.edu>
2023-04-24 15:24:04 -0400

    Aggregation
    Tried adding aggregation and broke the program. not sure how to fix it.

--- Policy.java
+++ Policy.java
@@ -3,46 +3,21 @@ public class Policy{
 
  //DATA FEILDS
    
-   int policyNumber;
-   String providerName;
-   String holderFirstName;
-   String holderLastName;
-   int holderAge;
-   String smokingStatus;
-   double holderHeight;
-   double holderWeight;
-   double insurancePrice;
+   private int policyNumber;
+   private String providerName;
+   private PolicyHolder ph; //instance of policyHolder class
+   private double insurancePrice;
 
-      //no arg constructor
       
-      public Policy()
-      {
-         policyNumber = 123456789;
-         providerName = "Blue Cross Blue Shield";
-         holderFirstName = "John";
-         holderLastName = "Doe";
-         holderAge = 69;
-         smokingStatus = "smoker";
-         holderHeight = 55;
-         holderWeight = 420;
-        
-      
-      }//end no arg const
       
       //constructor
       
-      public Policy(int polnum,String provName,String firstName,String lastName,int age,String smoke,double height,double weight)
+      public Policy(int polnum,String provName, PolicyHolder holder)
       {
          policyNumber = polnum;
          providerName = provName;
-         holderFirstName = firstName;
-         holderLastName = lastName;
-         holderAge = age;
-         smokingStatus = smoke;
-         holderHeight = height;
-         holderWeight = weight;
-      
-      }//end constructor
+         ph = new PolicyHolder(holder);     //this line breaks program despite having copied syntax from book exactly...
+      }//end constructor                     // not sure how to fix
 
 
          //////////////SETTERS////////////////
@@ -78,72 +53,12 @@ public class Policy{
          
          */
          
-         
-         public void setHolderFirstName(String firstName)
+         public void setPolicyHolder(PolicyHolder polhold)
          {
-            holderFirstName = firstName;
+            ph = new PolicyHolder(polhold);
          }
          
-          /**
-         Sets policy holder's last name
-         @param lastName
-         
-         */
-         
-         
-         public void setHolderLastName(String lastName)
-         {
-            holderLastName = lastName;
-         }
-         
-          /**
-         Sets policy holder's age
-         @param age
-         
-         */
-         
-         
-         public void setHolderAge(int age)
-         {
-            holderAge = age;
-         }
-         
-          /**
-         Sets smoker status 
-         @param smoke
-         
-         */
-         
-         
-         public void setSmokingStatus(String smoke)
-         {
-            smokingStatus = smoke;
-         }
-         
-          /**
-         Sets policy holder's height 
-         @param height
-         
-         */
-         
-         
-         public void setHolderHeight(double height)
-         {
-            holderHeight = height;
-         }
-         
-          /**
-         Sets policy holder's weight
-         @param weight
-         
-         */
-         
-         
-         public void setHolderWeight(double weight)
-         {
-            holderWeight = weight;
-         }
-         
+                  
          /////////////GETTERS//////////////////
          
           /**
@@ -169,88 +84,14 @@ public class Policy{
             return providerName;
          }
          
-          /**
-         Gets and returns policy holder's frist name
-         @return holderFirstName
-         
-         */
-         
-         public String getHolderFirstName()
+         public PolicyHolder getPolicyHolder()
          {
-            return holderFirstName;
+            return new PolicyHolder(ph);
          }
          
-          /**
-         Gets and returns policy holder's last name
-         @return holderLastName
          
-         */
          
-         public String getHolderLastName()
-         {
-            return holderLastName;
-         }
-         
-          /**
-         Gets and returns policy holder's age
-         @return holderAge
-         
-         */
-         
-         public int getHolderAge()
-         {
-            return holderAge;
-         }
-         
-          /**
-         Gets and returns smoker status 
-         @return smokingStatus
-         
-         */
-         
-         public String getSmokingStatus()
-         {
-            return smokingStatus;
-         }
-         
-          /**
-         Gets and returns policy holder's height
-         @return holderHeight
-         
-         */
-         
-         public double getHolderHeight()
-         {
-            return holderHeight;
-         }
-         
-          /**
-         Gets and returns policy holder's weight
-         @return holderWeight
-         
-         */
-         
-         public double getHolderWeight()
-         {
-            return holderWeight;
-         }
-         
-         ///////////////////////method that returns BMI//////////////////
-         
-          /**
-         Gets and returns policy holder's BMI
-         @return BMI
-         
-         */
-         
-         public double getBMI()
-         {
-            double BMI = (holderWeight * 703) / (holderHeight * holderHeight);
-            return BMI;
-            
-         }  
-         
-         /////////////////////method that returns price of insurance//////////////////////
+                  /////////////////////method that returns price of insurance//////////////////////
          
           /**
          Gets and returns the insurance price 
@@ -263,21 +104,31 @@ public class Policy{
             double baseFee = 600;
            
             
-            if(holderAge > 50)
+            if(ph.getHolderAge() > 50)
             {
                insurancePrice = 75 + baseFee;
             }
-            else if(smokingStatus == "smoker")
+            else if(ph.getSmokingStatus() == "smoker")
             {
                insurancePrice = 100 + baseFee;
             }
-            else if(getBMI() > 35)
+            else if(ph.getBMI() > 35)
             {
-               double additionalFee = (getBMI() -35) * 20;
+               double additionalFee = (ph.getBMI() -35) * 20;
                insurancePrice = baseFee + additionalFee;
                
             }
             return insurancePrice;
             
          }
+         
+         //to string method
+         
+         public String toString()
+         {
+            return String.format("Policy Number: " + policyNumber + "\nProvider Name: " + providerName +
+                                 "\n Policy Holder Info :" + ph.toString());                        
+                              
+         }
+         
 }//end class
